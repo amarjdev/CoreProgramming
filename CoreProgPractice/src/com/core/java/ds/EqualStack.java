2a951448458a1c4d2dd85623f664f44970aeca6c
mundlamuri <mundlamuri@192.168.0.29>
2017-03-21 15:22:09 -0400

    test

--- CoreProgPractice/src/com/core/java/ds/EqualStack.java
+++ CoreProgPractice/src/com/core/java/ds/EqualStack.java
@@ -4,7 +4,7 @@ public class EqualStack {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My First Project to commit");
+		System.out.println("My First Project to commit!!");
 	}
 
 }
