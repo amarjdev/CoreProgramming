9a5c8eafb8d72fa8075052a79f31bb13da286657
mundlamuri <mundlamuri@192.168.0.29>
2017-03-21 13:41:13 -0400

    New Java Project to solve problems in hackerrank

---
+++ CoreProgPractice/src/com/core/java/ds/EqualStack.java
@@ -0,0 +1,10 @@
+package com.core.java.ds;
+
+public class EqualStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Project");
+	}
+
+}
